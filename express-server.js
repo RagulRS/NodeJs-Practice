b63166eec3b509a2be141eddbf2b8768451e4d10
RagulRS <ragulsingh6245@gmail.com>
2024-06-27 17:42:28 +0530

    first commit

---
+++ express-server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const server = express();
+let students = [{
+    id: "1",
+    name:'Rahul',
+    age:20,
+    gender:'Male'
+}]
+server.get('/students', (req, res) => {
+    res.send({students});
+});
+
+const port =7000;
+server.listen(port, ()=>{
+    console.log(Date().toString(),": server listineng on", port);
+});
