271563595edc1ed85c0cfbde151d601cbcdd562e
RagulRS <ragulsingh6245@gmail.com>
2024-06-27 17:53:49 +0530

    changes in package.json file

--- express-server.js
+++ express-server.js
@@ -11,7 +11,8 @@ server.get('/students', (req, res) => {
     res.send({students});
 });
 
-const port =7000;
+
+const port =3000;
 server.listen(port, ()=>{
     console.log(Date().toString(),": server listineng on", port);
 });
